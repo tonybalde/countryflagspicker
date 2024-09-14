5dcca77a81e9a20b52ae3765ae952a19ee239974
Tony Baldessari <tonybalde@hotmail.com>
2024-09-14 10:47:26 -0300

    dark mode ok

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,38 +1,11 @@
-// import styles from "./MainContent.module.css";
-
-// const MainContent = () => {
-//     return (  
-//         <>
-        
-//             <div className={styles.mainContentContainer}>
-//                 <input type="search" placeholder="Search for a country..."></input>
-//                 <div className={styles.dropdownCountries}>
-//                     <button className={styles.dropdownButton}>Filter by Region</button>
-//                     <div className={styles.dropdownContent}>
-//                     <a href="#item1">Africa</a>
-//                     <a href="#item2">America</a>
-//                     <a href="#item3">Asia</a>
-//                     <a href="#item4">Europe</a>
-//                     <a href="#item5">Oceania</a>
-//                     </div>
-//                 </div>
-
-//             </div>
-        
-
-//         </>
-//     );
-// }
- 
-// export default MainContent;
 import React from 'react';
 import styles from './MainContent.module.css';
 
 function MainContent({ isDarkMode }) {
   return (
     <div className={`${styles.mainContent} ${isDarkMode ? styles.darkMain : styles.lightMain}`}>
-      {/* Your main content goes here */}
-                      
+      
+            <div className={styles.mainContentContainer}>          
                  <input type="search" placeholder="Search for a country..."></input>
                  <div className={styles.dropdownCountries}>
                      <button className={styles.dropdownButton}>Filter by Region</button>
@@ -44,9 +17,7 @@ function MainContent({ isDarkMode }) {
                      <a href="#item5">Oceania</a>
                      </div>
                  </div>
-
-
-      
+            </div>
     </div>
   );
 }
